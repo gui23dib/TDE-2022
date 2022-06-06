c1645d6b93a2f5099cca54cc3659ac81762390d4
Guilherme Dib <gui23dib@gmail.com>
2022-06-06 16:33:37 -0300

    06062022
    as coluna tao funcionando so falta mudar de linha

--- csvmain.c
+++ csvmain.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 #include "csvmain.h"
 
 #define SUCCES 0
 #define ERROR 1
 
 #define FILE_NAME "dados_covid_sp.csv"
-#define LINE_LENGTH 401
-#define MAX_COLUMNS 26
+#define LINE_LENGTH 901
+#define MAX_COLUMNS 28
 #define SEP ";"
-#define MAX_LINES 6000
+#define MAX_LINES 527200
 
 int openfile(FILE *csvfile){
+
+    /*setlocale(LC_ALL, "UTF-8");*/
 
     csvfile = fopen(FILE_NAME, "r"); /* modo "r" de abertura permite um arquivo de texto para leitura */
 
@@ -27,22 +30,60 @@ int openfile(FILE *csvfile){
 
     }*/
 
-    char linha[LINE_LENGTH], *cabecalho[MAX_COLUMNS], primeira_linha[LINE_LENGTH], * valor;
-    int n_colunas = 0, i, n_linhas = 0, valores_contados = 0;
+    char line[LINE_LENGTH], *header[MAX_COLUMNS], firstline[LINE_LENGTH], *val;
+    int columns = 0, i, linenum = 0 /*n_linhas*/, count = 0 /*valores_contados*/;
 
-    /**LE {RIMEIRA LINHA**/
-    fscanf(csvfile,"%s\n",linha);
+    /**LE PRIMEIRA LINHA**/
+    fscanf(csvfile, "%s\n" ,line);
 
     /**Separa o cabecalho**/
-    cabecalho[n_colunas] = strtok(linha, SEP);
-    while(cabecalho[n_colunas++] != NULL){
-        cabecalho[n_colunas] = strtok(NULL, SEP);
+    header[columns] = strtok(line, SEP);
+    while(header[columns++] != NULL){
+        header[columns] = strtok(NULL, SEP);
     }
 
     /**Mostra os valores do cabecalho**/
-    for (i = 0; i < n_colunas; i++){
-        printf("%s\n",cabecalho[i]);
+    for (i = 0; i < columns; i++){
+        if(i != columns-1){
+            printf("%s\n", header[i]);
+        }
     }
+
+    float media = 0;
+
+    while(fscanf(csvfile, "%900[^\n]", line)){
+
+        if(linenum == 0){
+            strcpy(firstline, line);
+        }
+
+        /*Primeira Coluna*/
+        val = strtok(line, SEP);
+
+        /*Mostra a primeira linha*/
+        printf("\nPrimeira coluna: %s\n", val);
+
+        /*Segunda Coluna*/
+        val = strtok(NULL, SEP);
+
+        printf("\nSegunda coluna: %s\n", val);
+
+
+        for(i = 0 ; i < 4 ; i++){  /*PULA DA TERCEIRA ATE A SETIMA COLUNA (CASOS NOVOS)*/
+            val = strtok(NULL, SEP);
+        }
+
+        printf("\nSetima coluna: %s\n", val);
+
+        linenum++;
+    }
+
+    /*Mostra a primeira linha*/
+    printf("\nPrimeira Linha: %s\n", firstline);
+
+    /*Mostra o numero de linhas*/
+    printf("\nNumero de linhas %d\n\n\n", linenum);
+
 
     getch();
     return SUCCES;
