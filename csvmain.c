72784f462c8a477f3d4a859bc0b94813461429cf
Guilherme Dib <gui23dib@gmail.com>
2022-06-07 16:51:32 -0300

    TESTE COM GETLINE FILTRANDO CODIGO DO IBGE (SO NA MAIN)

--- csvmain.c
+++ csvmain.c
@@ -15,38 +15,38 @@
 
 int openfile(FILE *csvfile){
 
-    /*setlocale(LC_ALL, "UTF-8");*/
     csvfile = fopen(FILE_NAME, "r"); /* modo "r" de abertura permite um arquivo de texto para leitura */
 
     if(csvfile == NULL) {
         printf("Erro! O arquivo nao foi aberto devidamente, ou nao foi encontrado...\n");
         return ERROR;
-
-    } /*else {
-        printf("O arquivo foi executado com sucesso!!!\n");
-        getch();
-        system("cls");
-        return SUCCES;
-
-    }*/
-
-    char line[LINE_LENGTH], *header[MAX_COLUMNS], firstline[LINE_LENGTH], *val;
-    int columns = 0, i, linenum = 0 /*n_linhas*/, count = 0 /*valores_contados*/;
-
-    // Ler todas as linhas do CSV
-    size_t tamanho = 0;
-    char* linha;
-    while(getline(&linha, &tamanho, csvfile)) {
-      printf("%s\n", linha);
     }
 
-    /**Separa o cabecalho**/
+    printf("Arquvio aberto com sucesso!!!\n");
+
+    /* Ler todas as linhas do CSV */
+    size_t tamanho = 0;
+    char* linha;
+    getline(&linha, &tamanho, csvfile);
+    printf("%zu\n", tamanho);
+    printf("%s\n", linha);
+
+
+
+
+
+
+
+
+
+
+
+    /*
     header[columns] = strtok(line, SEP);
     while(header[columns++] != NULL){
         header[columns] = strtok(NULL, SEP);
     }
 
-    /**Mostra os valores do cabecalho**/
     for (i = 0; i < columns; i++){
         if(i != columns-1){
             printf("%s\n", header[i]);
@@ -55,38 +55,38 @@ int openfile(FILE *csvfile){
 
     float media = 0;
 
-    while(fscanf(csvfile, "%900[^\n]", line)){
+    /*while(fscanf(csvfile, "%900[^\n]", line)){
 
         if(linenum == 0){
             strcpy(firstline, line);
         }
 
-        /*Primeira Coluna*/
+        Primeira Coluna
         val = strtok(line, SEP);
 
-        /*Mostra a primeira linha*/
+        Mostra a primeira linha
         printf("\nPrimeira coluna: %s\n", val);
 
-        /*Segunda Coluna*/
+        /*Segunda Coluna
         val = strtok(NULL, SEP);
 
         printf("\nSegunda coluna: %s\n", val);
 
 
-        for(i = 0 ; i < 4 ; i++){  /*PULA DA TERCEIRA ATE A SETIMA COLUNA (CASOS NOVOS)*/
+        for(i = 0 ; i < 4 ; i++){  PULA DA TERCEIRA ATE A SETIMA COLUNA (CASOS NOVOS)
             val = strtok(NULL, SEP);
         }
 
         printf("\nSetima coluna: %s\n", val);
 
         linenum++;
-    }
+    } */
 
-    /*Mostra a primeira linha*/
+    /*Mostra a primeira linha
     printf("\nPrimeira Linha: %s\n", firstline);
 
-    /*Mostra o numero de linhas*/
-    printf("\nNumero de linhas %d\n\n\n", linenum);
+    /*Mostra o numero de linhas
+    printf("\nNumero de linhas %d\n\n\n", linenum);*/
 
     getch();
     return SUCCES;
--- main.c
+++ main.c
@@ -8,7 +8,53 @@
 #define SUCCES 0
 #define ERROR 1
 
-int main()
+#define FILENAME "dados_covid_sp.csv"
+
+int main(void)
+{
+  /* Open the file for reading */
+  char *line_buf = NULL;
+  size_t line_buf_size = 0;
+  int line_count = 0;
+  size_t line_size;
+  FILE *fp = fopen(FILENAME, "r");
+  if (!fp)
+  {
+    fprintf(stderr, "Error opening file '%s'\n", FILENAME);
+    return EXIT_FAILURE;
+  }
+
+    char type[30];
+
+    scanf("%s", &type);
+  /* Get the first line of the file. */
+  line_size = getline(&line_buf, &line_buf_size, fp);
+
+  /* Loop through until we are done with the file. */
+  while (line_size >= 0)
+  {
+    /* Increment our line count */
+    line_count++;
+
+    /* Show the line details */
+    if(strstr(line_buf, type))
+    printf("[%06d] %s", line_count, line_buf);
+
+    /* Get the next line */
+    line_size = getline(&line_buf, &line_buf_size, fp);
+  }
+
+  /* Free the allocated line buffer */
+  free(line_buf);
+  line_buf = NULL;
+
+  /* Close the file now that we are done with it */
+  fclose(fp);
+
+  return EXIT_SUCCESS;
+}
+
+/*int main()
 {
     FILE *csvfile;
 
@@ -17,4 +63,4 @@ int main()
     menu(csvfile);
 
     return 0;
-}
+}*/
