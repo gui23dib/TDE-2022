47e081236eaf965803667f014ca0523014c2cbe1
Guilherme Dib <gui23dib@gmail.com>
2022-06-10 15:16:05 -0300

    RELEASE

--- csvmain.c
+++ csvmain.c
@@ -10,6 +10,9 @@
 #define FILENAME "dados_covid_sp.csv"
 #define SEP ";"
 
+/** DOWNLOAD DO DATASET: https://github.com/seade-R/dados-covid-sp/blob/master/data/dados_covid_sp.csv **/
+/** DOWNLOAD DO ARQUIVO DE VERIFICACAO: https://drive.google.com/file/d/1DZSjPVwj5mnbCjrjvvGa-b3IR-WJDf_t/view?usp=sharing **/
+
 int openfile(FILE *csvfile){
 
     setlocale(LC_ALL, "");
