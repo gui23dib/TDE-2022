105c83f6de3e83b9973c2ebc39580a5c602fc147
Guilherme Dib <gui23dib@gmail.com>
2022-11-15 23:55:05 -0300

    MERGE FUNCIONAL NAO INTEGRADO

--- merge.c
+++ merge.c
@@ -14,10 +14,14 @@
 #define true 1
 #define false 0
 
-void verificar_arquivo(FILE *fp){ 
+int arquivos_apagados = 0;
+
+int main(void);
+
+void verificar_arquivo(FILE *fp, char *filename){ 
    /*checa se o retorno do arquivo e nulo*/
    if(fp == NULL) { 
-      printf("Erro! O arquivo nao foi aberto devidamente, ou nao foi encontrado...\n");
+      printf("Erro! O arquivo %s nao foi aberto devidamente, ou nao foi encontrado...\n", filename);
       system("pause");
       exit(EXIT_FAILURE); /*desliga o programa*/
    }
@@ -26,7 +30,7 @@ void verificar_arquivo(FILE *fp){
 int contar_linhas(char *filename) {                                 
   FILE *fp = fopen(filename,"r");
    int ch=0, linhas=0;
-   verificar_arquivo(fp);
+   verificar_arquivo(fp, filename);
    do{
       ch = fgetc(fp);
       if(ch == '\n') linhas++;
@@ -55,33 +59,54 @@ void apagar_primeira_linha(char * nome_subarquivo){
    rename("temp", nome_subarquivo);
 }
 
+int apagar_arquivo_vazio(char * nome_subarquivo){
+   remove(nome_subarquivo);
+   char nome_subarquivo_velho[26];
+   char nome_subarquivo_novo[26];
+
+   if(atoi(nome_subarquivo) == (D_NUMERO_SUBARQUIVOS-arquivos_apagados)){
+      return 0;
+   }
+
+   for(int i = (atoi(nome_subarquivo)+1) ; i < (D_NUMERO_SUBARQUIVOS-arquivos_apagados) ; i++){
+      sprintf(nome_subarquivo_velho, "%d", i);
+      sprintf(nome_subarquivo_novo, "%d", i-1);
+      rename(nome_subarquivo_velho, nome_subarquivo_novo);
+   }
+   arquivos_apagados++;
+   return 0;
+}
+
 const char* ler_primeira_linha(int num_subarquivo){
    char nome_subarquivo[26];
    sprintf(nome_subarquivo, "%d", num_subarquivo);
 
    if(contar_linhas(nome_subarquivo) == 0){
-      printf("VAZIO! ");
-      return "-\n";
+      apagar_arquivo_vazio(nome_subarquivo);
+      printf("Apagando arquivo %i... \n%i arquivos apagados no total!\n", num_subarquivo, arquivos_apagados);
+      return "---";
+   } else {
+      FILE* subarquivo_atual = fopen(nome_subarquivo, "r+");
+         char *line = NULL;   /*ponteiro do conteudo da linha lida*/
+         size_t len = 0;   
+         ssize_t read;  /*tamanho da linha lida*/
+         read = getline(&line, &len, subarquivo_atual);
+      fclose(subarquivo_atual);
+      return line;
    }
-
-   FILE* subarquivo_atual = fopen(nome_subarquivo, "r+");
-      char *line = NULL;   /*ponteiro do conteudo da linha lida*/
-      size_t len = 0;   
-      ssize_t read;  /*tamanho da linha lida*/
-      read = getline(&line, &len, subarquivo_atual);
-   fclose(subarquivo_atual);
-
-   return line;
 }
 
 const char* torneio_arvore(){
-   char conteudo_subarquivos[D_NUMERO_SUBARQUIVOS][MAX_LINE]; 
+   char conteudo_subarquivos[D_NUMERO_SUBARQUIVOS-arquivos_apagados][MAX_LINE]; 
    int i;
 
    char conteudo_subarquivo_seguro[TAMANHO_SUBARQUIVOS][MAX_LINE]; /*dado descartado da separacao de colunas*/
    char conteudo_subarquivo_coluna[TAMANHO_SUBARQUIVOS][MAX_LINE];
 
-   for(i = 0 ; i < D_NUMERO_SUBARQUIVOS ; i++){
+   for(i = 0 ; i < D_NUMERO_SUBARQUIVOS-arquivos_apagados ; i++){
+      if(strcmp(ler_primeira_linha(i), "---") == 0){
+         continue;
+      }
       strcpy(conteudo_subarquivos[i], ler_primeira_linha(i));
       strcpy(conteudo_subarquivo_seguro[i], conteudo_subarquivos[i]);
 
@@ -93,7 +118,7 @@ const char* torneio_arvore(){
    char* menor_numero = conteudo_subarquivo_coluna[0];
    int int_arquivo_vencedor = 0;
 
-   for(i = 0 ; i < D_NUMERO_SUBARQUIVOS ; i++){
+   for(i = 0 ; i < D_NUMERO_SUBARQUIVOS-arquivos_apagados ; i++){
       if(atoi(conteudo_subarquivo_coluna[i]) < atoi(menor_numero)){
          menor_numero = conteudo_subarquivos[i];
          int_arquivo_vencedor = i;
@@ -105,18 +130,19 @@ const char* torneio_arvore(){
    sprintf(nome_subarquivo_vencedor, "%d", int_arquivo_vencedor);
    printf("SUBARQUIVO VENCEDOR: %s\n", nome_subarquivo_vencedor);
    apagar_primeira_linha(nome_subarquivo_vencedor);
-   printf("%s\n", conteudo_vencedor);
 
    return conteudo_vencedor; /*retorna o conteudo da linha lida*/
 }
 
 int main(void) {
    int linhas_arquivo_final;
-   const int total_linhas_soma_subarquivos = (((D_NUMERO_SUBARQUIVOS-1)*TAMANHO_SUBARQUIVOS) + (contar_linhas("4")+1));
+   char nome_ultimo_subarquivo[26];
+   sprintf(nome_ultimo_subarquivo, "%d", D_NUMERO_SUBARQUIVOS-arquivos_apagados-1);
+   const int total_linhas_soma_subarquivos = (D_NUMERO_SUBARQUIVOS-1)*TAMANHO_SUBARQUIVOS + contar_linhas(nome_ultimo_subarquivo)+1;
    const int total_linhas_arquivo_base = contar_linhas(ARQUIVO_BASE);
    
    FILE*fp = fopen(ARQUIVO_FINAL,"w");
-   verificar_arquivo(fp);
+   verificar_arquivo(fp, ARQUIVO_FINAL);
    fclose(fp);
 
    do{
@@ -124,8 +150,8 @@ int main(void) {
       fprintf(fp, torneio_arvore());
       fclose(fp);
       linhas_arquivo_final = contar_linhas(ARQUIVO_FINAL);
-      printf("%i\n", linhas_arquivo_final);
    }while(linhas_arquivo_final < total_linhas_soma_subarquivos);
+   printf("\nO ARQUIVO FINAL ESTA COMPLETAMENTE ORDENADO!\n");
    /*while para verificar que arquvio final tenha um numero de linhas igual ao numero total de linhas de todos os subarquivos*/
 
    return EXIT_SUCCESS;
